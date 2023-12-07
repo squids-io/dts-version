a3f55c75be0c54d7fd6039e2aee13f43020476c3
lixiao2924 <18838514278@163.com>
2023-12-07 11:58:08 +0800

    update images

--- updateimages.sh
+++ updateimages.sh
@@ -1,9 +1,9 @@
-for name in dbmotion:2310-hcs-oem dbmotion-ui:2310-hcs-oem kafka:hcs-oem
+for name in dbmotion:2310-hcs-oem dbmotion-ui:zstack-2310.01 kafka:hcs-oem phoenix:1.4.7-dbmotion-hcs-oem
 do
 docker pull "${1}${name}"
 docker tag "${1}${name}" $name
 docker push $name
-for ip in 10.10.66.25 10.10.66.26 10.10.66.27;
+for ip in 10.10.30.146 10.10.30.219;
 do
 docker tag $name $ip:30081/irds/$name
 docker push  $ip:30081/irds/$name
